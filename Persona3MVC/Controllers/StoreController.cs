853eedd0cbe5becf08b1c2b5c51d835af84aff50
asd asd <seonghyejin33@gmail.com>
2024-08-18 13:58:23 +0700

    commit frontend finish

---
+++ Persona3MVC/Controllers/StoreController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persona3MVC.Models;
+using Persona3MVC.Services;
+
+namespace Persona3MVC.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly int pageSize = 4;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index(int pageIndex,string? search, string? arcana, int? level, string? sort)
+        {
+            IQueryable<Persona> query = context.Personas;
+
+
+
+            if (search != null && search.Length > 0)
+            {
+                query = query.Where(p => p.Name.Contains(search));
+            }
+
+
+            // filter functionality
+            if (arcana != null && arcana.Length > 0)
+            {
+                query = query.Where(p => p.Arcana.Contains(arcana));
+            }
+
+            if (level != null )
+            {
+                query = query.Where(p => p.Level<=level);
+            }
+
+            // sort functionality
+            if (sort == "price_asc")
+            {
+                query = query.OrderBy(p => p.Price);
+            }
+            else if (sort == "price_desc")
+            {
+                query = query.OrderByDescending(p => p.Price);
+            }
+            else
+            {
+                // newest products first
+                query = query.OrderByDescending(p => p.Id);
+            }
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            var personas = query.ToList();
+            ViewBag.Personas = personas;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+            var storeSearchModel = new StoreSearchModel()
+            {
+                Search = search,
+                Arcana = arcana,
+                Level = level,
+                Sort = sort
+            };
+            return View(storeSearchModel);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var persona = context.Personas.Find(id);
+            if (persona == null)
+            {
+                return RedirectToAction("Index", "Store");
+            }
+
+            return View(persona);
+        }
+    }
+}
